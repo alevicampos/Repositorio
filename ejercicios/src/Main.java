4c44be48f5f34e4ae1f2e725524447c06272dd05
A601\1DAW <alevicampos@hotmail.com>
2024-09-25 20:37:07 +0200

    Programa suma

--- ejercicios/src/Main.java
+++ ejercicios/src/Main.java
@@ -1,16 +1,44 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("¿Como Te llamas?");
+
+        Scanner entrada = new Scanner(System.in);
+
+        String nombre = entrada.next();
+
+        System.out.println("¿A qué curso vas?");
+
+        String curso = entrada.next();
+
+        System.out.println("Hola " + nombre + " bienvenido a " + curso);
+
+        System.out.println("Introduce el primer número:");
+
+        int num1 = entrada.nextInt();
+
+        System.out.println("Introduce el segundo número:");
+
+        int num2 = entrada.nextInt();
+
+        System.out.println("La suma de "+num1+ " y " + num2 + " es igual a " + (num1+num2));
 
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
