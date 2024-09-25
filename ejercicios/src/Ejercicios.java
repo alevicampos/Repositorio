da2a0b93602f2d3d8b989d1fbda28b710912ef9b
A601\1DAW <alevicampos@hotmail.com>
2024-09-25 20:51:05 +0200

    Algunos cambios

--- ejercicios/src/Ejercicios.java
+++ ejercicios/src/Ejercicios.java
@@ -1,9 +1,31 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args) {
-        Ejercicios ejercicio = new Ejercicios ();
-        ecercicio.ejercicio1();
+public class Ejercicios{
+
+    public void actividad_inicial() {
+
+        System.out.println("¿Como Te llamas?");
+
+        Scanner entrada = new Scanner(System.in);
+
+        String nombre = entrada.next();
+
+        System.out.println("¿A qué curso vas?");
+
+        String curso = entrada.next();
+
+        System.out.println("Hola " + nombre + " bienvenido a " + curso);
+
+        System.out.println("Introduce el primer número:");
+
+        int num1 = entrada.nextInt();
+
+        System.out.println("Introduce el segundo número:");
+
+        int num2 = entrada.nextInt();
+
+        System.out.println("La suma de "+num1+ " y " + num2 + " es igual a " + (num1+num2));
+
     }
 
 
--- ejercicios/src/Main.java
+++ ejercicios/src/Main.java
@@ -6,28 +6,9 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("¿Como Te llamas?");
+       Ejercicios ejercicio = new Ejercicios();
 
-        Scanner entrada = new Scanner(System.in);
-
-        String nombre = entrada.next();
-
-        System.out.println("¿A qué curso vas?");
-
-        String curso = entrada.next();
-
-        System.out.println("Hola " + nombre + " bienvenido a " + curso);
-
-        System.out.println("Introduce el primer número:");
-
-        int num1 = entrada.nextInt();
-
-        System.out.println("Introduce el segundo número:");
-
-        int num2 = entrada.nextInt();
-
-        System.out.println("La suma de "+num1+ " y " + num2 + " es igual a " + (num1+num2));
-
+       ejercicio.actividad_inicial();
 
 
 
